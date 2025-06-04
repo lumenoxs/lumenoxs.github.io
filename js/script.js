ba6e09fe99f4a91804763035b0c79aa790baf7a0
lumenoxs <alexreinthx@gmail.com>
2025-06-04 11:23:50 +0100

    Added js files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openDiscord() {
+    window.open("https://discord.gg/dmREN9BRtY", "_blank");
+}
+function openGitHub() {
+    window.open("https://THIS_IS_UNDER_BETA.com", "_blank");
+}
+function openEmail() {
+    window.open("mailto:owner@rpicpu.dev", "_blank");
+}
