77e90e37cd01fe09da8ed13d7d66bf4b91ba60ca
lumenoxs <190932586+lumenoxs@users.noreply.github.com>
2025-07-19 17:56:21 +0100

    Removed external links (will be added back later)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function openDiscord() {
-    window.open("https://discord.gg/dmREN9BRtY", "_blank");
+    window.open("https://THIS_IS_UNDER_BETA.com", "_blank");
 }
 function openGitHub() {
     window.open("https://THIS_IS_UNDER_BETA.com", "_blank");
 }
 function openEmail() {
-    window.open("mailto:owner@rpicpu.dev", "_blank");
+    window.open("https://THIS_IS_UNDER_BETA.com", "_blank");
 }
